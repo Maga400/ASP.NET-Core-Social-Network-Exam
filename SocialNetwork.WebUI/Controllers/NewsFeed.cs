68480432ee3969efe9d4e2b8d337b342e2fb620c
Hemze_jx83@itstep.edu.az <Hemze_jx83@itstep.edu.az>
2024-09-18 08:13:09 +0400

    Add project files.

---
+++ SocialNetwork.WebUI/Controllers/NewsFeed.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialNetwork.WebUI.Controllers
+{
+    public class NewsFeed : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
