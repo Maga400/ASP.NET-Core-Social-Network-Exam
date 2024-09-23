0711482b999d5103a96091187eb60fae63777456
Hemze_jx83@itstep.edu.az <Hemze_jx83@itstep.edu.az>
2024-09-23 19:22:50 +0400

    finish logn register design

--- SocialNetwork.WebUI/Program.cs
+++ SocialNetwork.WebUI/Program.cs
@@ -12,7 +12,7 @@ using SocialNetwork.WebUI.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddHttpContextAccessor();
+//builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddControllersWithViews();
 
